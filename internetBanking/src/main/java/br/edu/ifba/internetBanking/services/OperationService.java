659fcfaeda715a7fe83b2405c58b908489fb166b
lucasaafonso <lucasaafonso103@gmail.com>
2025-07-28 15:46:23 -0300

    Operações

--- internetBanking/src/main/java/br/edu/ifba/internetBanking/services/OperationService.java
+++ internetBanking/src/main/java/br/edu/ifba/internetBanking/services/OperationService.java
@@ -1,7 +1,11 @@
 package br.edu.ifba.internetBanking.services;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.edu.ifba.internetBanking.dtos.AccountDTO;
 import br.edu.ifba.internetBanking.dtos.OperationDTO;
 import br.edu.ifba.internetBanking.entitys.Operation;
 import br.edu.ifba.internetBanking.repositorys.OperationRepository;
@@ -19,5 +23,21 @@ public class OperationService {
         operation = operationRepository.save(operation);
         
         return new OperationDTO(operation);
+    }
+
+    public OperationDTO deposit(AccountDTO accountDTO, BigDecimal value) {
+        return null;
+    }
+
+    public OperationDTO withdrawal(AccountDTO accountDTO, BigDecimal value) {
+        return null;
+    }
+
+    public OperationDTO payment(AccountDTO accountDTO, BigDecimal value) {
+        return null;
+    }
+
+    public List<OperationDTO> statement() {
+        return null;
     }
 }
