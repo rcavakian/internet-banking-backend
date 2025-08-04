25fd9ee0fab35886d8afeae90a7aa509f677dad8
lucasaafonso <lucasaafonso103@gmail.com>
2025-08-04 15:43:05 -0300

    Correção de warning

--- internetBanking/src/main/java/br/edu/ifba/internetBanking/entities/User.java
+++ internetBanking/src/main/java/br/edu/ifba/internetBanking/entities/User.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import br.edu.ifba.internetBanking.dtos.UserDTO;
 import br.edu.ifba.internetBanking.dtos.UserForm;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
